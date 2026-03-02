7aa653f55406fed97da073317397479355ca6cb4
Eray G. <88250653+eraygdev@users.noreply.github.com>
2026-03-02 21:55:34 +0300

    Refactor main.go by removing existing code
    Removed existing implementation of main function and related structures.

--- main.go
+++ main.go
@@ -5,13 +5,11 @@ import (
 	"fmt"
 )
 
-// Product: Kütüphanedeki Book yapısı gibi
 type Product struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
 
-// Table: Masalarımızı temsil eder
 type Table struct {
 	ID     int       `json:"id"`
 	IsFull bool      `json:"is_full"`
@@ -19,19 +17,6 @@ type Table struct {
 }
 
 func main() {
-	fmt.Println("eraygdev POS Sistemi - Backend v0.1")
-
-	// Örnek bir masa oluşturalım
-	masa1 := Table{
-		ID:     1,
-		IsFull: true,
-		Orders: []Product{
-			{Name: "Pizza", Price: 350.0},
-			{Name: "Ayran", Price: 40.0},
-		},
-	}
-
-	// JSON'a dönüştürüp ekrana basalım (C'de bunu yapmak çok zordu!)
-	jsonData, _ := json.MarshalIndent(masa1, "", "  ")
-	fmt.Println(string(jsonData))
+	
 }
+

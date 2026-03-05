481b16e5cfb75e3bab42fa17198a244f4da6a640
Eray G. <88250653+eraygdev@users.noreply.github.com.>
2026-03-06 00:24:59 +0300

    Update

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-
+	err := initRestaurants()
+	print(err)
 }
--- backend/restaurant_functions.go
+++ backend/restaurant_functions.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 )
 
 var Restaurants map[int]*Restaurant
+
+func (r *Restaurant) loadTables() error {
+	r.Tables = make(map[int]*Table)
+	TableAmount := r.Capacity
+
+	if TableAmount <= 0 {
+		return fmt.Errorf("Masa sayısı geçersiz!")
+	}
+
+	for i := 1; i <= TableAmount; i++ {
+		t := &Table{Number: i}
+		t.resetUncontrolled_unsafe()
+
+		r.Tables[i] = t
+	}
+
+	return nil
+}
 
 func (r *Restaurant) loadMenu() error {
 	data, err := os.ReadFile("menu.json")
@@ -13,33 +32,41 @@ func (r *Restaurant) loadMenu() error {
 		return err
 	}
 
-	var captured map[int][]MenuItem
-	error := json.Unmarshal(data, &captured)
-	if error != nil {
+	var captured map[int]map[int]*MenuItem
+	err1 := json.Unmarshal(data, &captured)
+	if err1 != nil {
+		return err1
+	}
+
+	for _, items := range captured {
+		if len(items) > 0 {
+			for itemid, item := range items {
+				if _, ok := r.Menu[itemid]; ok {
+					r.Menu[itemid].ID = itemid
+					r.Menu[itemid].Name = item.Name
+					r.Menu[itemid].Category = item.Category
+					r.Menu[itemid].PrepTime = item.PrepTime
+					r.Menu[itemid].Price = item.Price
+					r.Menu[itemid].Stock = item.Stock
+					r.Menu[itemid].IsActive = item.IsActive
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func initRestaurants() error {
+	data, err := os.ReadFile("restaurants_data.json")
+	if err != nil {
 		return err
 	}
 
-}
-
-func (r *Restaurant) loadTables() {
-	r.Tables = make(map[int]*Table)
-
-	TableAmount := r.Capacity
-	for i := 1; i <= TableAmount; i++ {
-		t := &Table{Number: i}
-		t.resetUncontrolled_unsafe()
-
-		r.Tables[i] = t
-	}
-}
-
-func init() {
-	data, err := os.ReadFile("restaurants_data.json")
-	println(err)
-
 	var captured map[int][]Restaurant
-	err2 := json.Unmarshal(data, &captured)
-	println(err2)
+	err1 := json.Unmarshal(data, &captured)
+	if err1 != nil {
+		return err1
+	}
 
 	for id, list := range captured {
 		if len(list) > 0 {
@@ -50,9 +77,15 @@ func init() {
 				r.Capacity = list[id].Capacity
 
 				// **Load child elements**
-				r.loadTables()
-				r.loadMenu()
+				if err := r.loadTables(); err != nil {
+					return err
+				}
+				if err := r.loadMenu(); err != nil {
+					return err
+				}
+				return nil
 			}
 		}
 	}
+	return nil
 }
